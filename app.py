a48892a6642f3643479eebcae3590e85528c7d2b
Dhrubojit Adhikari <dhrubojitgod@gmail.com>
2024-12-25 12:31:06 +0600

    github2 commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
 import json
